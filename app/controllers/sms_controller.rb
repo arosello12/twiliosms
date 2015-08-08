11c50693dd227706293d7e5fdae357fa3f7842ca
Alfred Rosello <alrosello@fusionworlds.com>
2015-08-08 14:26:26 -0400

    sms quickstart update

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -7,23 +7,36 @@ class SmsController < ApplicationController
   # Receive incoming SMS
   def smsincoming
   
-   # Test one
+   # Example 1
    # response = Twilio::TwiML::Response.new do |r|
    #     r.Message "Hey Monkey. Thanks for the message!"
    # end
    # render_twiml response
     
+   # Example 2 
+   # response = Twilio::TwiML::Response.new do |r|
+   #     r.Message do |message|
+   #    message.Body "Here are some images to view blah blah blah and Blah blah blah do re mi fa sol la ti"
+   #     message.Media "https://demo.twilio.com/logo.png"
+   #     message.Media "https://demo.twilio.com/owl.png"
+   #     message.Media "https://demo.twilio.com/owl.png"
+   #      message.Media "https://demo.twilio.com/logo.png"
+   #     end
+   # end
+   # render_twiml response
+
+   # Example 3
+    sender = params[:From]
+    friends = {
+      "+18137678172" => "Al Rosello",
+      "+13053063771" => "Better Voice",
+      "+14155551234" => "Virgil"
+    }
+    name = friends[sender] || "Mobile Monkey"
     response = Twilio::TwiML::Response.new do |r|
-        r.Message do |message|
-        message.Body "Here are some images to view blah blah blah and Blah blah blah do re mi fa sol la ti"
-        message.Media "https://demo.twilio.com/logo.png"
-        message.Media "https://demo.twilio.com/owl.png"
-        message.Media "https://demo.twilio.com/owl.png"
-         message.Media "https://demo.twilio.com/logo.png"
-        end
+      r.Message "Hello, #{name}. Thanks for the message."
     end
-   
-   render_twiml response
+    render_twiml response
 
   end
 
