85b15e8ed6846baa1660e5d9b701885522d101d5
Alfred Rosello <alrosello@fusionworlds.com>
2015-08-08 14:53:54 -0400

    sms quickstart update

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -8,13 +8,13 @@ class SmsController < ApplicationController
   # Receive incoming SMS
   def smsincoming
 
-   # Example 1
+   # Example 1 ------------------------------------------------------------------------------------
    # response = Twilio::TwiML::Response.new do |r|
    #     r.Message "Hey Monkey. Thanks for the message!"
    # end
    # render_twiml response
     
-   # Example 2 
+   # Example 2 ------------------------------------------------------------------------------------
    # response = Twilio::TwiML::Response.new do |r|
    #     r.Message do |message|
    #    message.Body "Here are some images to view blah blah blah and Blah blah blah do re mi fa sol la ti"
@@ -26,33 +26,37 @@ class SmsController < ApplicationController
    # end
    # render_twiml response
 
-   # Example 3
+   # Example 3 ------------------------------------------------------------------------------------
+   # sender = params[:From]
+   # friends = {
+   #   "+18137678172" => "Al Rosello",
+   #   "+13053063771" => "Better Voice",
+   #   "+14155551234" => "Virgil"
+   # }
+   # session["counter"] ||= 0
+   # sms_count = session["counter"]
 
-    sender = params[:From]
-    friends = {
-      "+18137678172" => "Al Rosello",
-      "+13053063771" => "Better Voice",
-      "+14155551234" => "Virgil"
-    }
-    session["counter"] ||= 0
-    sms_count = session["counter"]
+   # name = friends[sender] || "Mobile Monkey"
 
-    name = friends[sender] || "Mobile Monkey"
+   # if sms_count == 0
+   #   message = "Hello, #{name}. Thanks for the new message."
+   # else
+   #   message = "Hello, #{name}. Thanks for message number #{sms_count + 1}"
+   # end
 
-    if sms_count == 0
-      message = "Hello, #{name}. Thanks for the new message."
-    else
-      message = "Hello, #{name}. Thanks for message number #{sms_count + 1}"
-    end
+   # response = Twilio::TwiML::Response.new do |r|
+   #   r.Message message
+   # end
+   # session["counter"] += 1
+   # render_twiml response
 
-    response = Twilio::TwiML::Response.new do |r|
-      r.Message message
-    end
-    session["counter"] += 1
-    render_twiml response
-
+ # Example 4 - Sending Text Message From a Phone Call -----------------------------------------------
+   response = Twilio::TwiML::Response.new do |r|
+     r.Say "Hello Monkey! You will get an SMS message soon."
+     r.Sms "Here it is!" 
+   end 
+    render_twiml response 
   end
-
 
 
   private
